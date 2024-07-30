670503f1577a1f8ceb1aa10161ab29bf1e7e85c1
Samy ben beya <samyben2001@hotmail.com>
2024-07-30 11:38:08 +0200

    fonction saisie_colonne => Done

--- jeu.py
+++ jeu.py
@@ -7,10 +7,31 @@ def jouer():
     pass
 
 
+"""
+Demande à l'utilisateur de saisir une colonne valide dans laquelle jouer.
+Retourne la colonne saisie par l'utilisateur.
+"""
 def saisie_colonne():
-    """pour demander à l'utilisateur de saisir.."""
-    pass
+    # Indique si la saisie est valide ou non
+    isValid = False
 
+    # Boucle tant que la saisie n'est pas valide
+    while not isValid:
+        try:
+            # Demande à l'utilisateur de saisir une colonne
+            col = int(input(f"Saisissez la colonne dans laquelle vous voulez jouer (0-{COLONNES-1}) : "))
+
+            # Vérifie si la colonne est valide
+            if 0 <= col < COLONNES:
+                isValid = True
+                return col
+
+            # Affiche un message d'erreur si la colonne n'est pas valide
+            print(f"Veuillez entrer une colonne valide (0-{COLONNES-1})")
+        except ValueError:
+            # Affiche un message d'erreur si la saisie n'est pas un nombre
+            print("Veuillez entrer une valeur numérique")
+            
 
 def main():
     """Où le jeu commencera."""
